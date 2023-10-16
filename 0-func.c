6bfbf5c0ab9f70acc0c48e2adb8d66b337472184
Emeremnu Edison Maduabuchi <edisonemeremnu@gmail.com>
2023-10-16 19:53:06 +0000

    added print_number function

--- 0-func.c
+++ 0-func.c
@@ -32,3 +32,27 @@ int p_str(va_list p_args)
 	}
 	return (len);
 }
+
+/**
+  * print_number - print an integer
+  * @n: integer to be printed
+  */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if(n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
